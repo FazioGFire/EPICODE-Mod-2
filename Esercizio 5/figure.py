425aa2f92fb9004b06e107f16f72281176291219
FazioGFire <148903959+FazioGFire@users.noreply.github.com>
2023-11-30 20:37:32 +0100

    Update figure.py

--- Esercizio 5/figure.py
+++ Esercizio 5/figure.py
@@ -5,20 +5,16 @@ while 'y' in programLoop or 's' in programLoop or 'si' in programLoop:
     userInput = input()
 
     if 'qu' in userInput:
-        print("Stiamo a calcolare il quadrato. Damme il lato:")
-        lato = int(input())
+        lato = int(input("Stiamo a calcolare il quadrato. Damme il lato:"))
         print("L'area del quadrato è: " + str(lato*lato) + ". Il perimetro del rettangolo è: " + str(lato*4))
 
-    elif 'ret' in userInput:
-        print("Stiamo a fare il rettangolo. Damme la base")
-        base = int(input())
-        print("Mo damme l'altezza")
-        altezza = int(input())
+    elif 're' in userInput:
+        base = int(input("Stiamo a fare il rettangolo. Damme la base"))
+        altezza = int(input("Mo damme l'altezza"))
         print("L'area del rettangolo è:" +  str(base*altezza) + ". Il perimetro è:" +  str((base*2)+(altezza*2)) + ".")
 
     elif 'cer' in userInput:
-        print("Forse facciamo il cerchio. Damme il raggio.")
-        raggio = int(input())
+        raggio = int(input("Forse facciamo il cerchio. Damme il raggio."))
         pi = 3.14
         print("Il diametro del cerchio è: " + str((raggio*raggio)*pi) + ". la circonferenza del cerchio è: " + str(2*pi*(raggio*raggio)) + ".")
 
@@ -26,7 +22,6 @@ while 'y' in programLoop or 's' in programLoop or 'si' in programLoop:
         print("zio non so che dici")
         break
 
-    print("Vuoi fare un altro calcolo, zio? si/yes per confermare")
-    programLoop = input()
+    programLoop = input("Vuoi fare un altro calcolo, zio? si/yes per confermare")
 
 print("ciao.")
